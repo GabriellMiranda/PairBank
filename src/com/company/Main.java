5c75c7dc35d71d30f38ad7b8b68adeb01ea71d4d
Augusto-cmk <pedro.maia@ufv.br>
2021-12-14 13:17:35 -0300

    Inicio implementação da arvore de dados!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,19 @@ public class Main {
 
     public static void main(String[] args) {
         Administrador adm = new Administrador();
-        adm.cadastroADM();
+//        adm.cadastroADM();
+        Patricia arvore = new Patricia();
+//        arvore.insere("Amintas");
+//        arvore.insere("Carol");
+//        arvore.insere(adm);
+        arvore.insere(9);
+        arvore.insere(11);
+        arvore.insere(3);
+        arvore.insere(19);
+        arvore.insere(5);
+        arvore.insere(2);
+        int A = (int) arvore.busca(3);
+        System.out.print(A);
+//        arvore.imprime(arvore.root);
     }
 }
